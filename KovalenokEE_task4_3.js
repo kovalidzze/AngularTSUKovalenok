775e458a065960fb23b33bdcc4bdecfd8b4d9ca3
kovalidzze <katekovalenok@gmail.com>
2021-03-23 04:18:23 +0700

    Исправила замечания

--- KovalenokEE_task4_3.js
+++ KovalenokEE_task4_3.js
@@ -1,23 +1,31 @@
 function showArrayResult()
 {
     let array = document.getElementById('array_input').value;
+    let i = document.getElementById('i_input').value;
     console.log(array);
-    if (array.length == 0)
+    let realArray = array.split(',');
+    console.log(realArray.length);
+    console.log(realArray);
+    if (realArray.length == 0)
     {
         alert ("Ошибка, введите значения массива");
     }
-    else if (array.length < 7) //столько знаков с запятыми
+    else if (realArray.length < 4) 
     {
-        alert ("Ошибка, введите больше четырех значений через запятую");
+        alert ("Ошибка, введите больше значений через запятую, не менее четырех");
     }
     else {
-    let realArray = array.split(',');
-    console.log(realArray);
-    let result = realArray.slice(0,4);
+        if (i < 4 || i > realArray.length )
+        {
+            alert ("Ошибка, введите число не меньше 3 и не больше длины массива");
+        }
+        else {
+    let result = realArray.slice(0,i);
     console.log(result);
     let output = document.getElementById('array_output');
     output.innerText = result;
     //alert(result);
     }
+}
     
 }
