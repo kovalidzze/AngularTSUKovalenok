c0aeec9401d15271de698edb7bb04395aa2b2fff
kovalidzze <katekovalenok@gmail.com>
2021-03-23 23:15:18 +0700

    Вроде сделала 3 вариант 5 задания

---
+++ KovalenokEE_task5_3.js
@@ -0,0 +1,32 @@
+//Напишите функцию, которая поверхностно находит пересечения объектов и возвращает объект пересечений.
+//const data = { a: 1, b: 2 };
+//const data2 = { c: 1, b: 2 };
+//console.log(intersection(data, data2)); // { b: 2 }
+const data1 = { a: 1, b: 2 };
+const data2 = { c: 1, b: 2 };
+const intersection = (object1, object2) => 
+{
+    const keyObj1 = [];
+    for (let key in data1)
+      {
+        keyObj1.push(key);
+      }
+    //console.log(keyObj1);
+
+    const result = keyObj1.reduce((intersect = {}, key) => 
+    {
+      if (object1[key] === object2[key]) 
+      {
+        intersect = 
+        {
+          ...intersect,
+          [key]: object1[key],
+        };
+      }
+      return intersect;
+    }, 
+    {});
+    return result;
+  };
+
+console.log(intersection(data1, data2));
